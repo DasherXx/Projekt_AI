acd67071522d18bbf4c4d92c3109f2d840495561
DasherXx <koptamarcin@gmail.com>
2026-03-13 19:04:46 +0100

    Nauka zakonczona

--- aplikacja.cpp
+++ aplikacja.cpp
@@ -1,38 +1,35 @@
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
+#include <ctime>
 #include <iomanip>
+#include <vector>
 
 using namespace std;
 
-double sigmoid(double x) {
-    return 1.0 / (1.0 + exp(-x));
-}
-
-double sigmoid_pochodna(double x) {
-    return x * (1.0 -x);
-}
+double sigmoid(double x) { return 1.0 / (1.0 + exp(-x)); }
+double sigmoid_pochodna(double x) { return x * (1.0 - x); }
 
 int main() {
-        srand(time(0));
+    srand(time(0));
 
-        double wagi_wejscie[3][4];
-        double wagi_wyjscie[4];
-        double lr = 0.15;
+    double wagi_wejscie[3][4]; 
+    double wagi_wyjscie[4];    
+    double lr = 0.15; 
 
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 4; j++)
-                wagi_wejscie[i][j] = ((rand() % 2000) / 1000.0) - 1.0;
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 4; j++)
+            wagi_wejscie[i][j] = ((rand() % 2000) / 1000.0) - 1.0;
 
-        for (int j=0; j < 4; j++)
-            wagi_wyjscie[j] = ((rand() % 2000) / 1000.0) - 1.0;
+    for (int j = 0; j < 4; j++)
+        wagi_wyjscie[j] = ((rand() % 2000) / 1000.0) - 1.0;
 
-    cout << "Siec uczy sie zasady 'minimum dwie parzyste'..." << endl;
+    cout << "Siec sie uczy zasady 'minimum dwie parzyste'..." << endl;
 
-    for (int epoka = 0; epoka < 5000; epoka++) {
-        int l1 = rand() & 100;
-        int l2 = rand() & 100;
-        int l3 = rand() & 100;
+    for (int epoka = 0; epoka < 50000; epoka++) {
+        int l1 = rand() % 100;
+        int l2 = rand() % 100;
+        int l3 = rand() % 100;
 
         double x[3];
         x[0] = (l1 % 2 == 0) ? 1.0 : 0.0;
@@ -47,11 +44,12 @@ int main() {
             double suma = 0;
             for (int i = 0; i < 3; i++) suma += x[i] * wagi_wejscie[i][j];
             hidden[j] = sigmoid(suma);
+        }
 
         double output = 0;
         for (int j = 0; j < 4; j++) output += hidden[j] * wagi_wyjscie[j];
         output = sigmoid(output);
-        
+
         double error = target - output;
         double delta_output = error * sigmoid_pochodna(output);
 
@@ -59,6 +57,43 @@ int main() {
         for (int j = 0; j < 4; j++)
             delta_hidden[j] = delta_output * wagi_wyjscie[j] * sigmoid_pochodna(hidden[j]);
 
-        
+        for (int j = 0; j < 4; j++) {
+            wagi_wyjscie[j] += lr * delta_output * hidden[j];
+            for (int i = 0; i < 3; i++)
+                wagi_wejscie[i][j] += lr * delta_hidden[j] * x[i];
+        }
     }
+
+    cout << "Nauka zakonczona!" << endl;
+
+    while (true) {
+        int t1, t2, t3;
+        cout << "\nPodaj trzy liczby (np. 2 1 6) lub 0 0 0 by wyjsc: ";
+        cin >> t1 >> t2 >> t3;
+        if (t1 == 0 && t2 == 0 && t3 == 0) break;
+
+        double tx[3];
+        tx[0] = (t1 % 2 == 0) ? 1.0 : 0.0;
+        tx[1] = (t2 % 2 == 0) ? 1.0 : 0.0;
+        tx[2] = (t3 % 2 == 0) ? 1.0 : 0.0;
+
+        double h[4], out = 0;
+        for (int j = 0; j < 4; j++) {
+            double s = 0;
+            for (int i = 0; i < 3; i++) s += tx[i] * wagi_wejscie[i][j];
+            h[j] = sigmoid(s);
+            out += h[j] * wagi_wyjscie[j];
+        }
+        out = sigmoid(out);
+
+        cout << "------------------------------------" << endl;
+        cout << "Liczby: " << t1 << ", " << t2 << ", " << t3 << endl;
+        cout << "Interpretacja (P=1, N=0): " << tx[0] << ", " << tx[1] << ", " << tx[2] << endl;
+        cout << "Wynik sieci: " << (out > 0.5 ? "PRAWDA (min. 2 parzyste)" : "FALSZ") << endl;
+        cout << "Pewnosc: " << fixed << setprecision(2) << (out > 0.5 ? out * 100 : (1.0 - out) * 100) << "%" << endl;
+        cout << "Raw output: " << out << endl;
+        cout << "------------------------------------" << endl;
+    }
+
+    return 0;
 }
